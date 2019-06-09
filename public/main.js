fb66a9696696826665b8fef738e0f556c1770086
Stefan <stefancvetincanin@gmail.com>
2019-06-09 20:58:51 +0200

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -6,7 +6,14 @@ const reqBody = {
   p2: 'World!'
 }
 
-const options = {
+const addUserData = {
+  jmbg: '',
+  ime: '',
+  prezime: '',
+  adresa: ''
+}
+
+const optionsPost1 = {
   method: 'POST',
   headers: {
     'Content-Type': 'application/json'
@@ -15,30 +22,68 @@ const options = {
 }
 
 // POST REQUEST
-fetch('/table/t1', options)
-  .then(results => results.json())
-  .then(results => {
-    // results = JSON.stringify(results)
-    document.getElementById('test').innerHTML = `Poruka: ${results.body.p1} ${results.body.p2}`
-    // document.getElementById('test').innerHTML = results
-  })
+// fetch('/table/t1', optionsPost1)
+//   .then(results => results.json())
+//   .then(results => {
+//     // results = JSON.stringify(results)
+//     console.log(results)
+//     document.getElementById('test').innerHTML = `Poruka: ${results.body.p1} ${results.body.p2}`
+//     // document.getElementById('test').innerHTML = results
+//   })
+getUsers()
+
+// Dodavanje korisnika
+s('post-forma').addEventListener('submit', e => {
+  s('post-obavestenje').innerHTML = ''
+  e.preventDefault()
+  addUserData.jmbg = s('post-jmbg').value
+  addUserData.ime = s('post-ime').value
+  addUserData.prezime = s('post-prezime').value
+  addUserData.adresa = s('post-adresa').value
+  const optionsAddUser = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(addUserData)
+  }
+  fetch('/table/add', optionsAddUser)
+    .then(res => res.json())
+    .then(res => {
+      if(res.affectedRows === 1)
+        s('post-obavestenje').innerHTML = 'Podatak dodat u bazu'
+      getUsers()
+    })
+})
 
 // GET REQUEST
-// const broj1 = 5
-// const broj2 = 10
-// document.getElementById('test2').innerHTML += `Sabiramo brojeve ${broj1} i ${broj2}<br>`
-// fetch(`/table/?a=${broj1}&b=${broj2}`)
-//   .then(res => res.json())
-//   .then(res => {
-//     document.getElementById('test2').innerHTML += JSON.stringify(res)
-//   })
+// Nabavljanje liste polaznika
+function getUsers() {
+  fetch('/table/list')
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      let polazniciDisplay = ''
+      res.forEach(element => {
+        polazniciDisplay += `
+        <div class='polaznici'>
+          JMBG: ${element.jmbg}<br>
+          Ime: ${element.ime}<br>
+          Prezime: ${element.prezime}<br>
+          Adresa: ${element.adresa}<br>
+        </div>
+        `
+      })
+      s('polaznici-container').innerHTML = polazniciDisplay
+    })
+}
 
+// Pretraga po bazi
 s('searchPrezime').addEventListener('click', function(){
   const prezime = s('prezime').value
-  fetch(`/table/?prezime=${prezime}`)
+  fetch(`/table/?parametar=${prezime}`)
   .then(res => res.json())
   .then(res => {
-    // document.getElementById('test2').innerHTML = JSON.stringify(res)
     document.getElementById('test2').innerHTML = `
       JMBG: ${res[0].jmbg}<br>
       Ime: ${res[0].ime}<br>
@@ -49,11 +94,10 @@ s('searchPrezime').addEventListener('click', function(){
 })
 
 s('searchJmbg').addEventListener('click', function(){
-  const prezime = s('jmbg').value
-  fetch(`/table/?prezime=${prezime}`)
+  const jmbg = s('jmbg').value
+  fetch(`/table/?parametar=${jmbg}`)
   .then(res => res.json())
   .then(res => {
-    // document.getElementById('test2').innerHTML = JSON.stringify(res)
     document.getElementById('test2').innerHTML = `
       JMBG: ${res[0].jmbg}<br>
       Ime: ${res[0].ime}<br>
@@ -61,5 +105,28 @@ s('searchJmbg').addEventListener('click', function(){
       Adresa: ${res[0].adresa}<br>
     `
   })
+})
+
+// BRISANJE KORISNIKA
+s('delete-user-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  const removeUserData = {
+    jmbg: s('delete-jmbg').value
+  }
+  const optionsRemoveUser = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(removeUserData)
+  }
+  console.log(optionsRemoveUser)
+  fetch('/table/remove/kor1', optionsRemoveUser)
+    .then(res => res.json())
+    .then(res => {
+      console.log('Rezultat brisanja:')
+      console.log(res)
+      getUsers()
+    })
 })
 
--- router.js
+++ router.js
@@ -1,24 +1,46 @@
 module.exports = function (express, baza) {
   const router = express.Router()
 
-  // POST ZAHTEV
-  router.post('/table/*', function (req, res, next) {
-    console.log('Telo requesta: ', req.body)
-    console.log('Parametri requesta: ', req.params)
-    res.send({
-      p: 'SOME DATA - sent by POST response',
-      params: req.params,
-      body: req.body
+  // POST ZAHTEV - insert upit
+  router.post('/table/add', function (req, res, next) {
+    console.log(req.body)
+    baza.execQuery(`INSERT INTO polaznici(jmbg, ime, prezime, adresa) VALUES ('${req.body.jmbg}', '${req.body.ime}', '${req.body.prezime}', '${req.body.adresa}')`, function(results) {
+      res.send(results)
     })
   })
 
-  // GET ZAHTEV
-  router.get('/table', function (req, res, next) {
-    console.log('Upit requesta: ' + req.query.prezime)
-    baza.execQuery(`SELECT * FROM polaznici where prezime='${req.query.prezime}' OR jmbg='${req.query.prezime}'`, function(results){
+  // router.post('/table/t1', function (req, res, next) {
+  //   console.log('Telo requesta: ', req.body)
+  //   console.log('Parametri requesta: ', req.params)
+  //   res.send({
+  //     p: 'SOME DATA - sent by PRVI POST',
+  //     params: req.params,
+  //     body: req.body
+  //   })
+  // })
+
+  // GET ZAHTEV - select upit
+  router.get('/table', function (req, res) {
+    console.log('Parametar requesta: ' + req.query.parametar)
+    baza.execQuery(`SELECT * FROM polaznici where prezime='${req.query.parametar}' OR jmbg='${req.query.parametar}'`, function(results){
       res.send(results)
-      console.log(results)
     })
   })
+
+  router.get('/table/list', function (req, res) {
+    baza.execQuery(`SELECT * FROM polaznici`, function(results) {
+      res.send(results)
+    })
+  })
+
+  // DELETE ZAHTEV - delete upit
+  router.delete('/table/remove/*', function (req, res) {
+    console.log('Ispunjava delete zahtev, parametri: ' + req.params['0'] + ' jmbg: ' + req.body.jmbg)
+    // Izgleda moze da se brise i bez body-a u requestu, preko parametra
+    baza.execQuery(`DELETE FROM polaznici WHERE jmbg='${req.body.jmbg}'`, function(results) {
+      res.send(results)
+    })
+  })
+
   return router
 }
